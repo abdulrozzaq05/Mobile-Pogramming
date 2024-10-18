5b9f0cbef0ec6abbfda6166cd9fd486e38cc765b
abdulrozzaq05 <138552359+abdulrozzaq05@users.noreply.github.com>
2024-10-18 13:31:29 +0700

    rajaampat.dart

--- lib/rajaampat.dart
+++ lib/rajaampat.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'sewa.dart';
 
 class RajaAmpat extends StatelessWidget {
   const RajaAmpat({super.key});
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('PAKET LIBURAN RAJA AMPAT'),
@@ -23,6 +24,16 @@ class RajaAmpat extends StatelessWidget {
             Text(
               'PAKET LIBURAN RAJA AMPAT',
               style: TextStyle(fontSize: 22),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Sewa()),
+                );
+              },
+              child: const Text('Sewa Sekarang'),
             ),
           ],
         ),
