2a8a8942f7d371af6c686959d42e7f9f9012e4cc
abdulrozzaq05 <138552359+abdulrozzaq05@users.noreply.github.com>
2024-10-18 13:28:38 +0700

    banyuwangi.dart

--- lib/banyuwangi.dart
+++ lib/banyuwangi.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'sewa.dart';
 
 class Banyuwangi extends StatelessWidget {
   const Banyuwangi({super.key});
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('PAKET LIBURAN WISATA BANYUWANGI'),
@@ -23,6 +24,16 @@ class Banyuwangi extends StatelessWidget {
             Text(
               'PAKET LIBURAN WISATA BANYUWANGI',
               style: TextStyle(fontSize: 22),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Sewa()),
+                );
+              },
+              child: const Text('Sewa Sekarang'),
             ),
           ],
         ),
